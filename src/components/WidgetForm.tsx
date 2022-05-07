516fb172c02db69c9ffcfc1bd9e688d182baa909
JonathanAndrade19 <jonathanjampamb@gmail.com>
2022-05-07 17:38:44 -0300

    creating type feedback form

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -3,6 +3,7 @@ import { CloseButton } from "./CloseButton";
 import bugImageUrl from '../assets/bug.svg';
 import ideiaImageUrl from '../assets/ideia.svg';
 import thougthImageUrl from '../assets/thougth.svg';
+import { useState } from "react";
 
 const feedbackTypes = {
     BUG: {
@@ -28,7 +29,10 @@ const feedbackTypes = {
     }
 }
 
+type feedbackType = keyof typeof feedbackTypes;
+
 export function WidgetForm() {
+    const [feedbackType, setfeedbackType] = useState<feedbackType | null>(null)
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
             <header>
@@ -36,9 +40,23 @@ export function WidgetForm() {
                 <CloseButton />
             </header>
 
-            <div className="flex py-8 gap-2 w-full">
-
+           {!feedbackType ? ( 
+           <div className="flex py-8 gap-2 w-full">
+            { Object.entries(feedbackTypes).map(([key,value]) => {
+                return (
+                    <button
+                        key={key}
+                        className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+                        type="button"
+                        onClick={() => setfeedbackType(key as feedbackType)}
+                    >
+                        <img src={value.image.source} alt={value.image.alt} />
+                        <span>{value.title}</span>
+                    </button>
+                )
+            })}
             </div>
+            ) : ( <p>Hello World!</p> )}
 
             <footer className="text-xs text-neutral-400">
                 Feito com ♥ pela <a className="underline underline-offset-2" href="">Rocketseat</a>
